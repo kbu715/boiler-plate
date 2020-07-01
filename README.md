a2bc87c07693a9501ed6b67eab6e9b4d97913123
kbu715 <kbu715@gmail.com>
2020-07-01 14:05:29 +0900

    비밀 정보 보호

---
+++ README.md
@@ -0,0 +1,13 @@
+# boiler-plate
+
+# nodemon
+
+소스를 변경할 때 그걸 감지해서 자동으로 서버를 재시작해주는 tool
+
+npm install nodemon --save-dev : nodemon을 로컬에서만 사용할 수 있게 해준다.
+
+# Heroku 가입 / mongoURI 저장
+
+안한다. 짜증난다.
+
+
--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const config = require('./config/key')
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
   .connect(
-    "mongodb+srv://tiger:1234@cluster0-cgecu.mongodb.net/test?retryWrites=true&w=majority",
+    config.mongoURI,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -16,5 +24,25 @@ mongoose
   .then(() => console.log("MongoDB Connected..."))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello world"));
+app.get("/", (req, res) =>
+  res.send("Hello worlsdfsdfsdf")
+);
+
+app.post("/register", (req, res) => {
+  //회원 가입 할때  필요한 정보들을  client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body);
+
+  //mongodb에서 오는 method
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  }); //status(200) : 성공했다는 method
+});
+
 app.listen(port, () => console.log(`Example app Listening on port ${port}!`));
+
+//
